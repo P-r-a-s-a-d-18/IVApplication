5784495c6f4220be9f4ef8dabaab96171fe083e2
Prasad <prasadhiwarkar18@gmail.com>
2023-02-14 00:03:11 +0530

    Modified CompatibilityCheckResult.kt : 1. Logic part shifted to DrugViewModel.kt. 2. List of all possible selected drug-pairs displayed.

--- app/src/main/java/com/example/iv1/ui/CompatibilityCheckResult.kt
+++ app/src/main/java/com/example/iv1/ui/CompatibilityCheckResult.kt
@@ -1,7 +1,6 @@
 package com.example.iv1.ui
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,57 +8,48 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.iv1.data.Drug
+import com.example.iv1.data.DrugViewModel
 
-@OptIn(ExperimentalFoundationApi::class)
+
 @Composable
-fun StartCheck(viewModel: DrugViewModel) {
-    val drugList = viewModel.getSelectedDrugList()
-    val toCheck: ArrayList<Pair<Drug, Drug>> = ArrayList()
-    for(i in 0 until drugList.size - 1) {
-        for(j in i+1 until drugList.size) {
-            if(i != j) {
-                toCheck.add(Pair(drugList[i], drugList[j]))
-            }
-        }
-    }
-    LazyColumn {
-        toCheck.forEach { pair ->
-            stickyHeader {
-                Text(
-                    text = pair.first.drug_name.trim() + " -- " + pair.second.drug_name.trim(),
-                    color = Color.White,
-                    modifier = Modifier
-                        .background(Color.Gray)
-                        .padding(5.dp)
-                        .fillMaxWidth()
-                )
-            }
-            items(listOf(pair)) {drugPair ->
-                DisplayCheck(drugPair.first, drugPair.second)
+fun GetCheck(
+    viewModel: DrugViewModel,
+    onElementClicked: (Pair<Drug, Drug>) -> Unit
+) {
+    if (viewModel.tempList.isEmpty() || viewModel.tempList.size == 1) {
+        Text(text = "Not enough drugs selected to check compatibility !!", fontSize = MaterialTheme.typography.h5.fontSize)
+    } else {
+        LazyColumn {
+            items(viewModel.getToCheck()) { pair ->
+                DisplayItem(pair, onElementClicked, viewModel)
             }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun DisplayCheck(drug1: Drug, drug2: Drug) {
-
-    Column(modifier = Modifier
-        .fillMaxSize()
+fun DisplayItem(
+    pair: Pair<Drug, Drug>,
+    onElementClicked: (Pair<Drug, Drug>) -> Unit,
+    viewModel: DrugViewModel
+) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(18.dp)
         .wrapContentSize()
-        .padding(10.dp),
-    Arrangement.spacedBy(8.dp)) {
-        Text(text = "Mixture", fontSize = MaterialTheme.typography.h4.fontSize)
-        Text(text = drug1.type_of_incompatibility[drug2.drug_name]?.get(0)?.get("Mixture").toString(), fontSize = MaterialTheme.typography.h5.fontSize)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Type", fontSize = MaterialTheme.typography.h4.fontSize)
-        Text(text = drug1.type_of_incompatibility[drug2.drug_name]?.get(0)?.get("Type").toString(), fontSize = MaterialTheme.typography.h5.fontSize)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Effect", fontSize = MaterialTheme.typography.h4.fontSize)
-        Text(text = drug1.type_of_incompatibility[drug2.drug_name]?.get(0)?.get("Effect").toString(), fontSize = MaterialTheme.typography.h5.fontSize)
+        .clickable {
+            onElementClicked(pair)
+            viewModel.setPair(pair)
+        }
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = pair.first.drug_name + " -- " + pair.second.drug_name, fontSize = MaterialTheme.typography.h5.fontSize)
+            Text(text = " > ", fontSize = MaterialTheme.typography.h5.fontSize)
+        }
     }
 }
