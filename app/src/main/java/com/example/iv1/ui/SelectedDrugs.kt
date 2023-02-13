af6cf076cf92c3e02b31842ea9c975bf5c7060f2
Prasad <prasadhiwarkar18@gmail.com>
2023-02-14 00:09:15 +0530

    Modified SelectedDrugs.kt.

--- app/src/main/java/com/example/iv1/ui/SelectedDrugs.kt
+++ app/src/main/java/com/example/iv1/ui/SelectedDrugs.kt
@@ -12,10 +12,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.iv1.data.Drug
+import com.example.iv1.data.DrugViewModel
 
 @Composable
 fun ShowSelectedList(
@@ -25,62 +25,55 @@ fun ShowSelectedList(
     viewModel: DrugViewModel
 ) {
     Column(modifier = Modifier.padding(10.dp)) {
-        if (drugs.isEmpty() || drugs.size == 1) {
-            Text(
-                text = "Not enough drugs to perform compatibility check.",
-                textAlign = TextAlign.Center
-            )
-        } else {
-            LazyColumn {
-                items(drugs) { drug ->
-                    SelectedListItem(drug, viewModel)
+        LazyColumn {
+            items(drugs) { drug ->
+                SelectedListItem(drug, viewModel)
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Box(
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 7.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                OutlinedButton(
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                    onClick = { onCheckBtnClicked(drugs) }, modifier = Modifier
+                        .width(200.dp)
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = "Check Incompatibility",
+                        color = Color.White,
+                        style = TextStyle(fontSize = 16.sp)
+                    )
                 }
             }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.Bottom
+
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Box(
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 7.dp),
+                contentAlignment = Alignment.Center
             ) {
-                Box(
-                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 7.dp),
-                    contentAlignment = Alignment.Center
+                OutlinedButton(
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Blue
+                    ),
+                    onClick = { onCancelBtnClicked() }, modifier = Modifier
+                        .width(110.dp)
+                        .height(50.dp)
                 ) {
-                    OutlinedButton(
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color.Blue
-                        ),
-                        onClick = { onCheckBtnClicked(drugs) }, modifier = Modifier
-                            .width(200.dp)
-                            .height(50.dp)
-                    ) {
-                        Text(
-                            text = "Check Incompatibility",
-                            color = Color.White,
-                            style = TextStyle(fontSize = 16.sp)
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.width(1.dp))
-
-                Box(
-                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 7.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    OutlinedButton(
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color.Blue
-                        ),
-                        onClick = { onCancelBtnClicked() }, modifier = Modifier
-                            .width(110.dp)
-                            .height(50.dp)
-                    ) {
-                        Text(
-                            text = "Cancel",
-                            color = Color.White,
-                            style = TextStyle(fontSize = 17.sp)
-                        )
-                    }
+                    Text(
+                        text = "Cancel",
+                        color = Color.White,
+                        style = TextStyle(fontSize = 17.sp)
+                    )
                 }
             }
         }
