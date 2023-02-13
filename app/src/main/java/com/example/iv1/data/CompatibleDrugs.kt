4e23809e512339b8227f9a7eb5d77c10179030a0
Prasad <prasadhiwarkar18@gmail.com>
2023-02-14 00:10:34 +0530

    -- Deletion of unused data classes -> 1. CompatibleDrugs.kt 2. IVFluid.kt

--- app/src/main/java/com/example/iv1/data/CompatibleDrugs.kt
+++
@@ -1,3 +0,0 @@
-package com.example.iv1.data
-
-data class CompatibleDrugs(var y_site: ArrayList<String> = ArrayList())
--- app/src/main/java/com/example/iv1/data/IVFluid.kt
+++
@@ -1,3 +0,0 @@
-package com.example.iv1.data
-
-data class IVFluid(var flush: String = "", var solution: String = "")
