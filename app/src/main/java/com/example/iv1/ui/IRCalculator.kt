32d97cc358f3fc66f04bc91ba8666ab9eb8574ff
Prasad <prasadhiwarkar18@gmail.com>
2023-02-16 15:57:52 +0530

    Modified IRCalculator.kt: 1. Added drop-down menu to select drop-factor.

--- app/src/main/java/com/example/iv1/ui/IRCalculator.kt
+++ app/src/main/java/com/example/iv1/ui/IRCalculator.kt
@@ -1,21 +1,28 @@
 package com.example.iv1.ui
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import com.example.iv1.R
 import java.text.NumberFormat
 
@@ -30,13 +37,6 @@ fun IRScreen() {
         mutableStateOf("")
     }
     val time = timeInput.toDoubleOrNull() ?: 0.0
-
-    var factorInput by remember {
-        mutableStateOf("")
-    }
-    val dropFactor = factorInput.toDoubleOrNull() ?: 0.0
-
-    val ir = calculateIR(volume, time, dropFactor)
 
     val focusManager = LocalFocusManager.current
 
@@ -80,19 +80,23 @@ fun IRScreen() {
 
         Spacer(modifier = Modifier.height(12.dp))
 
-        EditNumberField(label = R.string.ir_drop_factor,
-            value = factorInput,
-            onValueChange = { factorInput = it },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = { focusManager.clearFocus(true) }
-            )
-        )
+//        EditNumberField(label = R.string.ir_drop_factor,
+//            value = factorInput,
+//            onValueChange = { factorInput = it },
+//            keyboardOptions = KeyboardOptions(
+//                keyboardType = KeyboardType.Number,
+//                imeAction = ImeAction.Done
+//            ),
+//            keyboardActions = KeyboardActions(
+//                onDone = { focusManager.clearFocus(true) }
+//            )
+//        )
+
+        val dropFactor = dropFactorMenu()
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        val ir = calculateIR(volume, time, dropFactor)
 
         Text(
             text = stringResource(id = R.string.ir_result, ir),
@@ -102,8 +106,11 @@ fun IRScreen() {
     }
 }
 
-fun calculateIR(volume: Double, time: Double, dropFactor: Double): String {
-    val ir = (volume / time) * dropFactor
+fun calculateIR(volume: Double, time: Double, dropFactor: Int): String {
+    var ir = (volume / time) * dropFactor
+    if (ir.isNaN()) {
+        ir = 0.0
+    }
     return NumberFormat.getNumberInstance().format(ir)
 }
 
@@ -126,5 +133,55 @@ fun EditNumberField(@StringRes label: Int,
         keyboardActions = keyboardActions,
         singleLine = true
     )
+}
+
+@Composable
+fun dropFactorMenu(): Int {
+    var mExpanded by remember { mutableStateOf(false) }
+
+    val mCities = listOf(20, 60)
+
+    var mSelectedText by remember { mutableStateOf("") }
+
+    var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(Modifier.padding(20.dp)) {
+
+        OutlinedTextField(
+            value = mSelectedText,
+            onValueChange = { mSelectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    mTextFieldSize = coordinates.size.toSize()
+                },
+            label = {Text("Drop Factor")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+        )
+        DropdownMenu(
+            expanded = mExpanded,
+            onDismissRequest = { mExpanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){mTextFieldSize.width.toDp()})
+        ) {
+            mCities.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    mSelectedText = label.toString()
+                    mExpanded = false
+                }) {
+                    Text(text = label.toString())
+                }
+            }
+        }
+    }
+    return mSelectedText.toIntOrNull()?: 0
 }
 
